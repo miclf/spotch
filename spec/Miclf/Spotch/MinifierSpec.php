bc1b39ba992e6f81f66f39d10c8d48417100df82
miclf <michael@estsurinter.net>
2014-09-08 23:46:16 +0200

    [test] Check if the class is initializable

--- spec/Miclf/Spotch/MinifierSpec.php
+++ spec/Miclf/Spotch/MinifierSpec.php
@@ -7,4 +7,11 @@ use Prophecy\Argument;
 
 class MinifierSpec extends ObjectBehavior
 {
+    /**
+     * Test that the class can be instantiated correctly.
+     */
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Miclf\Spotch\Minifier');
+    }
 }
