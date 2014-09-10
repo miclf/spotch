9594dd00e4736c024df8022e7068812468e50413
miclf <michael@estsurinter.net>
2014-09-10 20:13:47 +0200

    Add Minifier class and its phpspec specification

--- spec/Miclf/Spotch/MinifierSpec.php
+++ spec/Miclf/Spotch/MinifierSpec.php
@@ -1,9 +1,6 @@
-<?php
-
-namespace spec\Miclf\Spotch;
+<?php namespace spec\Miclf\Spotch;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class MinifierSpec extends ObjectBehavior
 {
@@ -13,5 +10,148 @@ class MinifierSpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType('Miclf\Spotch\Minifier');
+    }
+
+    public function it_trims_lines()
+    {
+        $this->trimLines("    var foo = 'bar';    ")
+        ->shouldReturn("var foo = 'bar';");
+    }
+
+    public function it_removes_empty_lines()
+    {
+        $this->removeEmptyLines("
+
+            var foo = 'bar';
+
+            var baz = 'boom';
+
+        ")
+        ->shouldReturn(
+"            var foo = 'bar';
+            var baz = 'boom';"
+        );
+    }
+
+    public function it_removes_new_line_characters()
+    {
+        $this->removeNewlines("
+
+var foo = 'bar';
+
+var baz = 'boom';"
+
+        )
+        ->shouldReturn(
+            "var foo = 'bar';var baz = 'boom';"
+        );
+    }
+
+    public function it_removes_single_line_comments_that_are_on_their_own_line()
+    {
+        $this->removeComments(
+           "// A comment on its own line.
+            var foo = 'bar';
+        ")->shouldReturn("
+            var foo = 'bar';
+        ");
+    }
+
+    public function it_removes_single_line_comments_at_the_end_of_a_line()
+    {
+        $this->removeComments("var foo = 'bar';// A comment after some code.")
+        ->shouldReturn("var foo = 'bar';");
+    }
+
+    public function it_does_not_accidentally_modify_urls()
+    {
+        $code = "'http://example.com';";
+
+        $this->minify($code)->shouldReturn($code);
+    }
+
+    public function it_does_not_accidentally_modify_regex()
+    {
+        $code = "/foo bar/;";
+
+        $this->minify($code)->shouldReturn($code);
+    }
+
+    public function it_removes_spaces_around_if()
+    {
+        $this->removeOptionalSpaces(' if (foo){return baz};')
+        ->shouldReturn('if(foo){return baz};');
+    }
+
+    public function it_removes_spaces_around_else()
+    {
+        $this->removeOptionalSpaces(' else {return baz};')
+        ->shouldReturn('else{return baz};');
+    }
+
+    public function it_removes_spaces_around_plus_signs()
+    {
+        $this->removeOptionalSpaces('foo  +  bar')
+        ->shouldReturn('foo+bar');
+    }
+
+    public function it_removes_spaces_around_equal_signs()
+    {
+        $this->removeOptionalSpaces('var foo = "bar";')
+        ->shouldReturn('var foo="bar";');
+    }
+
+    public function it_removes_spaces_around_double_equal_signs()
+    {
+        $this->removeOptionalSpaces('foo == "bar";')
+        ->shouldReturn('foo=="bar";');
+    }
+
+    public function it_removes_spaces_around_colons()
+    {
+        $this->removeOptionalSpaces('foo : "bar";')
+        ->shouldReturn('foo:"bar";');
+    }
+
+    public function it_removes_spaces_around_commas()
+    {
+        $this->removeOptionalSpaces('foo(bar, baz);')
+        ->shouldReturn('foo(bar,baz);');
+    }
+
+    public function it_removes_spaces_around_opening_parenthesis()
+    {
+        $this->removeOptionalSpaces('foo ( bar')
+        ->shouldReturn('foo(bar');
+    }
+
+    public function it_removes_spaces_around_closing_parenthesis()
+    {
+        $this->removeOptionalSpaces('foo ) bar')
+        ->shouldReturn('foo)bar');
+    }
+
+    public function it_removes_spaces_around_opening_curly_braces()
+    {
+        $this->removeOptionalSpaces(') { foo')
+        ->shouldReturn('){foo');
+    }
+
+    public function it_removes_spaces_around_closing_curly_braces()
+    {
+        $this->removeOptionalSpaces('foo } bar')
+        ->shouldReturn('foo}bar');
+    }
+
+    public function it_removes_spaces_around_and_operators()
+    {
+        $this->removeOptionalSpaces('foo && bar')
+        ->shouldReturn('foo&&bar');
+    }
+
+    public function it_removes_spaces_around_or_operators()
+    {
+        $this->removeOptionalSpaces('foo || bar')
+        ->shouldReturn('foo||bar');
     }
 }
---
+++ src/Minifier.php
@@ -0,0 +1,265 @@
+<?php namespace Miclf\Spotch;
+
+class Minifier
+{
+    /**
+     * Store strings that need to be protected
+     * from accidental modifications.
+     *
+     * @var array
+     */
+    protected $safe = [
+        'protocol' => [],
+        'regex'    => [],
+    ];
+
+    /**
+     * Regular expressions to identify and extract
+     * strings that need to be shielded.
+     *
+     * @var array
+     */
+    protected $shieldRegex = [
+        'protocol' => '#(\w+://)#U',
+        'regex'    => '#(replace\(/.+/)#U',
+    ];
+
+    /**
+     * Regular expressions to identify and remove
+     * optional space characters.
+     *
+     * @var array
+     */
+    protected $cleanRegex = [
+        '#\s*if\s*#'                 => 'if',
+        '#\s*else\s*#'               => 'else',
+        '#([^+])(\s*\+\s*)([^+])#'   => '$1+$3',
+        '#([^=])(\s*=\s*)([^=])#'    => '$1=$3',
+        '#([^=])(\s*==\s*)([^=])#'   => '$1==$3',
+        '#([^:])(\s*:\s*)([^:])#'    => '$1:$3',
+        '#([^,])(\s*,\s*)([^,])#'    => '$1,$3',
+        '#([^{])(\s*\{\s*)([^{])#'   => '$1{$3',
+        '#([^}])(\s*\}\s*)([^}])#'   => '$1}$3',
+        '#([^(])(\s*\(\s*)([^(])#'   => '$1($3',
+        '#([^)])(\s*\)\s*)([^)])#'   => '$1)$3',
+        '#([^&])(\s*&&\s*)([^&])#'   => '$1&&$3',
+        '#([^|])(\s*\|\|\s*)([^|])#' => '$1||$3',
+    ];
+
+    /**
+     * Minify a string of JavaScript code.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function minify($source)
+    {
+        // We start by extracting special strings from the code
+        // in order to prevent any accidental modification.
+        $code = $this->shield($source);
+
+        // Then, we remove the comments, since they’re
+        // not needed by the interpreter.
+        $code = $this->removeComments($code);
+
+        // We do some optional optimizations by removing
+        // space characters in different situations.
+        $code = $this->removeOptionalSpaces($code);
+
+        // Finally, we clean lines by trimming them and
+        // removing empty ones.
+        $code = $this->trimLines($code);
+        $code = $this->removeEmptyLines($code);
+        $code = $this->removeNewlines($code);
+
+        // To conclude, we reinject the special strings
+        // in the minified code and return it.
+        return $this->unshield($code);
+    }
+
+    /**
+     * Remove comments from a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function removeComments($source)
+    {
+        $lines = $this->splitLines($source);
+
+        $lines = array_map([$this, 'removeSingleLineComment'], $lines);
+
+        return $this->joinLines($lines);
+    }
+
+    /**
+     * Extract strings that need to be protected from
+     * accidental modifications.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    protected function shield($source)
+    {
+        foreach ($this->shieldRegex as $type => $pattern) {
+            $source = $this->shieldPattern($source, $pattern, $type);
+        }
+
+        return $source;
+    }
+
+    /**
+     * Protect substrings matching a pattern
+     * from accidental modifications.
+     *
+     * @param  string  $source
+     * @param  string  $pattern
+     * @param  string  $type
+     * @return string
+     */
+    protected function shieldPattern($source, $pattern, $type)
+    {
+        return preg_replace_callback($pattern, function ($matches) use ($type) {
+
+            $this->safe[$type][] = $matches[1];
+
+            return "__{$type}__";
+
+        }, $source);
+    }
+
+    /**
+     * Reinject strings that were shielded.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    protected function unshield($source)
+    {
+        foreach ($this->shieldRegex as $type => $pattern) {
+            $source = $this->unshieldPattern($source, $type);
+        }
+
+        return $source;
+    }
+
+    /**
+     * Reinject substrings matching a given pattern.
+     *
+     * @param  string  $source
+     * @param  string  $type
+     * @return string
+     */
+    protected function unshieldPattern($source, $type)
+    {
+        $pattern = "#__{$type}__#U";
+
+        return preg_replace_callback($pattern, function ($matches) use ($type) {
+
+            return array_shift($this->safe[$type]);
+
+        }, $source);
+    }
+
+    /**
+     * Split a string to an array of lines.
+     *
+     * @param  string  $source
+     * @return array
+     */
+    protected function splitLines($source)
+    {
+        return preg_split('#[\n\r]+#', $source);
+    }
+
+    /**
+     * Concatenate an array of strings to a single string.
+     *
+     * @param  array   $lines
+     * @param  string  $glue
+     * @return string
+     */
+    protected function joinLines(array $lines, $glue = "\n")
+    {
+        return implode($glue, $lines);
+    }
+
+    /**
+     * Remove single line comments from a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    protected function removeSingleLineComment($source)
+    {
+        $comment = mb_strpos($source, '//');
+
+        if ($comment !== false) {
+            $source = substr($source, 0, $comment);
+        }
+
+        return $source;
+    }
+
+    /**
+     * Remove empty lines from a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function removeEmptyLines($source)
+    {
+        $lines = $this->splitLines($source);
+
+        $lines = array_filter($lines, function ($line) {
+            return (bool) trim($line);
+        });
+
+        return $this->joinLines($lines);
+    }
+
+    /**
+     * Trim individual lines in a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function trimLines($source)
+    {
+        $lines = $this->splitLines($source);
+
+        $lines = array_map(function ($line) {
+            return trim($line);
+        }, $lines);
+
+        return $this->joinLines($lines);
+    }
+
+    /**
+     * Remove newline characters from a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function removeNewlines($source)
+    {
+        $lines = $this->splitLines($source);
+
+        return trim($this->joinLines($lines, ''), "\n\r");
+    }
+
+    /**
+     * Remove optional spaces from a string.
+     *
+     * @param  string  $source
+     * @return string
+     */
+    public function removeOptionalSpaces($source)
+    {
+        foreach ($this->cleanRegex as $pattern => $replacement) {
+            $source = preg_replace($pattern, $replacement, $source);
+        }
+
+        return $source;
+    }
+}
